b8850f248e245d3251e9adaa66b4533323c48b2d
Asisipho <missfente@gmail.com>
2023-04-07 13:17:50 +0200

    done with scramber

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // get references to the button, input and output area
-var wordElement = document.querySelector('.word');
+var userInputElement = document.querySelector('.userInput');
 var scrambleBtn = document.querySelector('.scramble-btn');
 var outputElement = document.querySelector('.output');
 
 // function to scramble word 
 function scrambler() {
 
-    // get value of word 
-    var word = wordElement.value;
-    // split the letters of the word 
-    var letters = word.split('');
+    // get value of user input 
+    var input = userInputElement.value;
+    // split the input  
+    var split_input = input.split('');
     alert(letters)
     //variable to store the current letter
     hold_current = '';
     // loop through the length of the array using length of letters
-    for (var i = 0; i < letters.length; i++) {
+    for (var i = 0; i < split_input.length; i++) {
         //randomize the indexes of the letters so we can assign randomly the position  of each letters
         var rand_num = Math.floor(Math.random() * i);
 
-        hold_current = letters[i];
-        letters[i] = letters[rand_num];
-        letters[rand_num] = hold_current;
+        hold_current = split_input[i];
+        split_input[i] = split_input[rand_num];
+        split_input[rand_num] = hold_current;
     }
     //create a var to hold the new word 
     // used join becuase it concatinates string elements in into one string
 
-    var new_word = letters.join("");
+    var new_word = split_input.join("");
     // display my new word on html
     outputElement.innerHTML = new_word;
 }
